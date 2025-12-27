4d582ea3ea22b7d4c380c19b859dec688e0a3ed3
anoopreddy-padi <anoopreddy.padi@gmail.com>
2025-12-27 12:08:35 +0530

    homecontroller

--- src/main/java/com/example/books/Controller/HomeController.java
+++ src/main/java/com/example/books/Controller/HomeController.java
@@ -1,15 +1,15 @@
-// package com.example.books.Controller;
+package com.example.books.Controller;
 
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// @RestController
-// @RequestMapping("")
-// public class HomeController {
+@RestController
+@RequestMapping("")
+public class HomeController {
 
-//     @GetMapping("")
-//     public String homePage(){
-//         return "Welcome to Home page";
-//     }
-// }
+    @GetMapping("")
+    public String homePage(){
+        return "Welcome to Home page";
+    }
+}
